250ba3150339ed6658d11a86f822e170658e17a4
D Mack <dmack6464@gmail.com>
2019-12-17 18:17:38 +1100

    Add files via upload

--- index.js
+++ index.js
@@ -6,3 +6,30 @@ function closeAlert() {
     x.style.display = "none";
   }
 }
+
+function toggleSidebar() {
+  var x = document.getElementById("container");
+  var y = document.getElementById("sidebar");
+  if (x.style.paddingLeft === "0px") {
+    x.style.paddingLeft = "201px";
+    y.style.display = "block";
+  } else {
+    x.style.paddingLeft = "0px";
+    y.style.display = "none";
+  }
+}
+function toggleSidebarSide() {
+	var x = document.getElementById("container");
+	var y = document.getElementById("sidebar");
+	if (y.style.right === "0px") {
+		x.style.paddingRight = "unset";
+		x.style.paddingLeft = "201px";
+		y.style.right = "unset";
+		y.style.left = "0";
+	} else {
+		x.style.paddingRight = "201px";
+		x.style.paddingLeft = "unset";
+		y.style.right = "0";
+		y.style.left = "unset";
+	}
+}
