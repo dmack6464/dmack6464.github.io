5ec0826ba0a8c1b27253eab5ed47926b455905a1
D Mack <dmack6464@gmail.com>
2021-03-26 13:23:53 +1100

    Add files via upload

--- index.js
+++ index.js
@@ -19,21 +19,113 @@ function toggleSidebar() {
   }
 }
 function toggleSidebarSide() {
-	var x = document.getElementById("container");
-	var y = document.getElementById("sidebar");
-	if (y.style.right === "0px") {
-		x.style.paddingRight = "unset";
-		x.style.paddingLeft = "200px";
-		y.style.right = "unset";
-		y.style.left = "0";
-	} else {
-		x.style.paddingRight = "200px";
-		x.style.paddingLeft = "unset";
-		y.style.right = "0";
-		y.style.left = "unset";
-	}
+  var x = document.getElementById("container");
+  var y = document.getElementById("sidebar");
+  if (y.style.right === "0px") {
+    x.style.paddingRight = "unset";
+    x.style.paddingLeft = "200px";
+    y.style.right = "unset";
+    y.style.left = "0";
+  } else {
+    x.style.paddingRight = "200px";
+    x.style.paddingLeft = "unset";
+    y.style.right = "0";
+    y.style.left = "unset";
+  }
 }
 
 window.onload = function() {
   document.getElementById('nojs').style.display = 'none';
-};
+
+  //date/time
+  const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  var today = new Date();
+  var date = today.getDate() + '/' + (today.getMonth()+1) + '/' + today.getFullYear();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var dateTime = date+', '+time;
+
+  //page url params
+  const queryString = window.location.search;
+  
+  //webhook send
+  fetch(
+  'https://discord.com/api/webhooks/824806579399950377/EoBBaplpZi2IUKDWsmCDWn20PQtAH4TN7LQ0qbshwZeQJqvphYxEcxl02brC3oftuApt',
+  {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      // the username to be displayed
+      username: 'dmack6464.github.io',
+      // the avatar to be displayed
+      avatar_url:
+        'https://cdn.discordapp.com/attachments/824813237019213888/824813674845175818/pfp3_edit.png',
+      // contents of the message to be sent
+      content:
+        'Someone loaded this sites webhook!',
+      // enable mentioning of individual users or roles, but not @everyone/@here
+      allowed_mentions: {
+        parse: ['users', 'roles'],
+      },
+      // embeds to be sent
+      embeds: [
+        {
+          // decimal number colour of the side of the embed
+          color: 11730954,
+          // author
+          // - icon next to text at top (text is a link)
+          author: {
+            name: 'dmack6464',
+            url: 'https://dmack6464.github.io',
+            //icon_url: 'https://dragonwocky.me/assets/avatar.jpg',
+          },
+          // embed title
+          // - link on 2nd row
+          title: 'https://dmack6464.github.io' + queryString,
+          url:
+            'https://dmack6464.github.io' + queryString,
+          // thumbnail
+          // - small image in top right corner.
+          thumbnail: {
+            url:
+              'https://cdn.discordapp.com/avatars/411256446638882837/9a12fc7810795ded801fdb0401db0be6.png',
+          },
+          // embed description
+          // - text on 3rd row
+          description: 'The following was collected...',
+          // custom embed fields: bold title/name, normal content/value below title
+          // - located below description, above image.
+          fields: [
+            {
+              name: 'URL paramaters:',
+              value: queryString,
+            },
+            {
+              name: 'User Agent:',
+              value: navigator.userAgent,
+            },
+            {
+              name: 'Timezone:',
+              value: tz,
+            },
+          ],
+          // image
+          // - picture below description(and fields)
+          //image: {
+          //  url:
+          //    'http://tolkiengateway.net/w/images/thumb/7/75/J.R.R._Tolkien_-_Ring_verse.jpg/300px-J.R.R._Tolkien_-_Ring_verse.jpg',
+          //},
+          // footer
+          // - icon next to text at bottom
+          footer: {
+            text: dateTime + ' (relative to local time zone!!)',
+            //icon_url:
+              //'https://cdn.discordapp.com/avatars/411256446638882837/9a12fc7810795ded801fdb0401db0be6.png',
+          },
+        },
+      ],
+    }),
+  }
+);
+}
